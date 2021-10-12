6c52b9e0ccb69b3121ccc7aa934f4e450e275367
Al-Alloush <ahmad.alloush@gmail.com>
2021-10-12 08:32:13 +0200

    start java

---
+++ src/s3/HelloWorld.java
@@ -0,0 +1,12 @@
+package s3;
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world! in one line");
+		System.out.print("1) Hello world! in same line.");
+		System.out.print("2) hello world! in the same line");
+
+	}
+
+}
